4221454b12af808587e05a1dff93b22e0d21a083
han254 <peternjenga1706@gmail.com>
2022-06-22 01:29:53 +0300

    add top & bottom animations

--- app/src/main/java/com/moringaschool/hanshowtime/MainActivity.java
+++ app/src/main/java/com/moringaschool/hanshowtime/MainActivity.java
@@ -4,13 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+//        image = findViewById(R.id.imageView);
+        logo =findViewById(R.id.textView);
+        slogan =findViewById(R.id.textshowt);
+
+//        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
     }
 }
